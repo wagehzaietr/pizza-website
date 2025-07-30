965787f601a77dab4f9c159a0e0ab6d06b9b0387
wagehzaietr <bad1game1@hotmail>
2025-07-30 16:47:02 +0400

    fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
 
 
-import HeroSection from "@/components/hero-section";
+
 import React from "react";
 import BestSellers from "@/components/best-sellers";
 import AboutPage from "./about/page";
 import WhyChooseUs from "@/components/WhyChooseUs";
+import dynamic from "next/dynamic";
+import GlobalLoading from "./loading";
+
+const HeroSection = dynamic(() => import("@/components/hero-section"), {
+  loading: () => <GlobalLoading />,
+});
 
 function page() {
   return (
